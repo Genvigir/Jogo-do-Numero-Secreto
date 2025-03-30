c85e9bfdfdb515c62a365b3c6ab1be2fbbfa237e
Genvigir <lucasgenvigir@alunos.utfpr.edu.br>
2025-03-30 18:17:28 -0300

    Readequação do Código para consulta

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto  = 5 ;
-console.log(numeroSecreto)
+
+// Adicione um console.log para verificar o valor de "chute" após a entrada do usuário
 let chute = prompt('Escolha um número entre 1 e 10');
+console.log('Valor do chute:', chute);
+
+let numeroSecreto  = 5 ;
+
+// Adicione um console.log para verificar a comparação entre "chute" e "numeroSecreto"
+console.log('Resultado da comparação:', chute == numeroSecreto);
 
 // se chute for igual ao número secreto 
 if (chute == numeroSecreto){
     alert(`Isso ai! Você descobriu o número ${numeroSecreto}`);
 } else {
-    alert('Você errou!')
+    // Adicione um console.log para verificar o valor de "numeroSecreto" quando o jogador erra
+    console.log('Valor do número secreto:', numeroSecreto);
+    alert('O número secreto era ' + numeroSecreto);
 }
+
+//APAGAR CONSOLE.LOG NO FINAL DO PROJETO
